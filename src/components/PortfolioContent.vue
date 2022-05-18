a34a71ca2d7d2740eb65437cd9f4c7c4cdae4610
Severino Miguel <severino.miguel.119@ufrn.edu.br>
2022-05-18 14:50:46 -0300

    🚧 🎨 Esttrutura e organização da página

---
+++ src/components/PortfolioContent.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="content d-flex">
+    <div class="content__base d-flex">
+      <about-me />
+    </div>
+    <div class="d-none d-md-flex content__aside">
+      <a href="https://www.linkedin.com/in/severino-miguel/" target="_blank"
+        ><v-icon>mdi-linkedin</v-icon></a
+      >
+      <a href="https://github.com/migueloten" target="_blank"
+        ><v-icon>mdi-github</v-icon></a
+      >
+      <a href="https://wa.me/5584998014877" target="_blank"
+        ><v-icon>mdi-whatsapp</v-icon></a
+      >
+      <a href="https://twitter.com/migueloten1" target="_blank"
+        ><v-icon>mdi-twitter</v-icon></a
+      >
+      <a href="https://www.instagram.com/miguel_neto1/" target="_blank"
+        ><v-icon>mdi-instagram</v-icon></a
+      >
+      <a
+        href="https://drive.google.com/file/d/1Ap3X6evJR_IUK0Jlhzczs8_TMXbLGOuf/view?usp=sharing"
+        target="_blank"
+        ><v-icon>mdi-file-account</v-icon></a
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+import AboutMe from "./content/AboutMe.vue";
+export default {
+  name: "PortfolioPage",
+  components: { AboutMe },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+
+.content {
+  justify-content: space-evenly;
+
+  &__aside {
+    position: fixed;
+    bottom: 25px;
+    right: 35px;
+    flex-direction: column;
+    flex-wrap: nowrap;
+    align-items: center;
+    align-content: center;
+    justify-content: center;
+
+    a {
+      text-decoration: none;
+      color: $cultured;
+
+      &:hover {
+        color: $sky-blue-crayola;
+      }
+
+      .v-icon {
+        margin-top: 25px;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/PortfolioPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container class="ma-0 pa-0">
+    <header-page />
+    <portfolio-content />
+  </v-container>
+</template>
+
+<script>
+import HeaderPage from "./HeaderPage.vue";
+import PortfolioContent from "./PortfolioContent.vue";
+
+export default {
+  components: { HeaderPage, PortfolioContent },
+  name: "PortfolioPage",
+
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss" scoped>
+.v-container {
+  max-width: 100%;
+}
+</style>
