e3a47e67a70748a33b56c40000713df243510c4c
Severino Miguel <severino.miguel.119@ufrn.edu.br>
2022-05-18 14:49:33 -0300

    🎨 Criação do componente do cabeçalho

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <template>
   <v-app>
     <v-main>
-      <HelloWorld/>
+      <PortfolioPage />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PortfolioPage from "./components/PortfolioPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    PortfolioPage,
   },
 
   data: () => ({
     //
   }),
-}
+};
 </script>
+
+<style lang="scss">
+@import "./assets/scss/variables.scss";
+@import url("https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@100;200;300;400;600;700;800&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap");
+
+html,
+body {
+  background-color: $oxford-blue;
+  font-family: "poppins", sans-serif;
+}
+
+#app {
+  font-family: "poppins", sans-serif;
+}
+
+.v-application {
+  background-color: $oxford-blue;
+  color: $cultured;
+
+  a {
+    color: $sky-blue-crayola;
+  }
+}
+</style>
---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,194 @@
+<template>
+  <v-container class="header">
+    <!-- logo no header -->
+    <v-img class="my-auto" src="../assets/logo.png" />
+    <!-- lista de menu no header -->
+    <ol class="menu my-auto d-none d-md-flex">
+      <li v-for="(item, index) in items" :key="index">
+        <a :href="item.href"> {{ item.title }} </a>
+      </li>
+    </ol>
+  </v-container>
+  <!-- menu header - responsivo -->
+  <v-navigation-drawer
+    class="d-md-none"
+    v-model="drawer"
+    elevation="23"
+    color="#001b2e"
+    width="400"
+    temporary
+    position="right"
+  >
+    <v-list>
+      <div class="listOptions">
+        <div class="HeaderOption" v-for="(item, index) in items" :key="index">
+          <v-icon>mdi-{{ item.icon }}</v-icon>
+          <a :href="item.href"> {{ item.title }} </a>
+        </div>
+      </div>
+    </v-list>
+  </v-navigation-drawer>
+  <v-main class="d-md-none" style="height: 50px">
+    <div class="d-flex justify-center align-center h-100">
+      <v-btn
+        flat
+        icon
+        class="btn-responsivo"
+        size="large"
+        @click.stop="drawer = !drawer"
+        @click="overlayStyle"
+      >
+        <v-icon>mdi-{{ drawer ? "close" : "menu" }}</v-icon>
+      </v-btn>
+    </div>
+  </v-main>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+
+  created() {
+    window.addEventListener("scroll", this.onScroll);
+  },
+
+  data() {
+    return {
+      drawer: null,
+      items: [
+        { title: "Sobre mim", href: "#aboutMe", icon: "account" },
+        { title: "Currículo", href: "#resume", icon: "file-account" },
+        { title: "Repositórios", href: "#repositories", icon: "github" },
+        { title: "Contato", href: "#contact", icon: "email" },
+      ],
+    };
+  },
+
+  methods: {
+    overlayStyle() {
+      if (this.drawer) {
+        setTimeout(() => {
+          document.getElementsByClassName(
+            "v-navigation-drawer__scrim"
+          )[0].style.backgroundColor = "transparent";
+          document.getElementsByClassName(
+            "v-navigation-drawer__scrim"
+          )[0].style.opacity = "unset";
+          document.getElementsByClassName(
+            "v-navigation-drawer__scrim"
+          )[0].style.backdropFilter = "blur(10px)";
+        }, 10);
+      }
+    },
+
+    onScroll() {
+      if (document.documentElement.scrollTop > 50) {
+        document.getElementsByClassName("header")[0].style.height = "50px";
+        document.getElementsByClassName("btn-responsivo")[0].style.top = "92%";
+      } else {
+        document.getElementsByClassName("header")[0].style.height = "120px";
+        document.getElementsByClassName("btn-responsivo")[0].style.top = "30px";
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+
+.header {
+  background-color: transparentize($color: #000000, $amount: 0.93);
+  backdrop-filter: blur(10px);
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+  position: fixed;
+  top: 0;
+  max-width: 100% !important;
+  margin: 0;
+  padding: 5px;
+  min-height: 50px;
+  height: 120px;
+  transition: height 0.5s;
+  z-index: 1;
+
+  .v-img {
+    width: 10%;
+  }
+
+  .menu {
+    display: flex;
+    justify-content: space-evenly;
+    width: 80%;
+
+    li {
+      color: $sky-blue-crayola;
+      font-size: 0.8rem;
+
+      a {
+        color: $light-gray;
+        text-decoration: none;
+        font-size: 0.8rem;
+
+        &:hover {
+          color: $sky-blue-crayola;
+        }
+      }
+    }
+  }
+}
+
+.v-navigation-drawer {
+  padding: 8px 35px;
+  background-color: $oxford-blue !important;
+
+  .v-list {
+    background-color: transparent;
+    height: 100%;
+
+    .listOptions {
+      height: 100%;
+      text-align: center;
+      padding-top: 40%;
+
+      .HeaderOption {
+        display: flex;
+        align-items: center;
+        align-content: center;
+        flex-direction: column;
+        justify-content: center;
+        height: 20%;
+        color: $sky-blue-crayola;
+
+        .v-icon {
+          font-size: 1.8rem;
+        }
+
+        a {
+          color: $light-gray;
+          text-decoration: none;
+
+          &:hover {
+            color: $sky-blue-crayola;
+          }
+        }
+      }
+    }
+  }
+}
+
+.btn-responsivo.v-btn {
+  position: fixed;
+  right: 40px;
+  top: 30px;
+  transition: top 1s;
+  z-index: 1010;
+  background-color: transparent;
+  color: $sky-blue-crayola;
+
+  .v-icon {
+    font-size: 40px;
+  }
+}
+</style>
